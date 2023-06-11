4b9e1ef2a6f44cf82b08a761c84a51722f4cc455
magmesser <magmesser@gmail.com>
2023-06-11 15:34:09 -0400

    Fixed book jumbotron laptop size

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -63,7 +63,7 @@ const Publications = () => {
     // Main Section
     <div
       name="publications"
-      className="w-full md:h-screen lg:h-screen bg-[#274C77] text-[#E7ECEF] p-4"
+      className="w-full bg-[#274C77] text-[#E7ECEF] p-4"
     >
       {/* Main Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
@@ -78,11 +78,11 @@ const Publications = () => {
 
         {/* Book Jumbotron */}
         <div
-          className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center border-solid border border-[#E7ECEF] "
+          className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center border-solid border border-[#E7ECEF] jumbo_div"
           style={coverPhoto}
         >
           <div
-            className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+            className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed "
             style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
           >
             <div className="flex h-full items-center justify-center">
